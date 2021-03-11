e6f29435ac7ee9fc0c93b4054d3a4628d7c7e9b4
Laurie Burchell <laurie@snotra.inf.ed.ac.uk>
2021-03-11 16:20:59 +0000

    updated so it works with the new version of torchtext

--- tree-labeller/run-deen-pretrain.sh
+++ tree-labeller/run-deen-pretrain.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # test run duplicating Shu et al. with my implementation
+# we need to pretrain the model without any bottleneck
 date=$(date '+%d-%m-%Y-%H:%M:%S')
 
-python3 run.py --model_name tree2code_wmt14_${date}_ \
+python3 run.py --model_name tree2code_wmt14_${date}_pretrain_ \
 	--source_corpus wmt14.de.sp.filtered \
 	--target_corpus wmt14.en.sp.filtered \
 	--source_vocab wmt14.de.sp.vocab \
 	--target_trees wmt14.en.cfg.filtered \
 	--target_tree_vocab wmt14.treelstm.vocab \
 	--opt_limit_tree_depth 2 \
-	--opt_limit_datapoints 800000 \
-	--opt_codebits 8 \
-	--load_pretrain tree2code_wmt14_11-03-2021-11:53:56_pretrain_codebits-0_limit_datapoints-100000_limit_tree_depth-2 \
-        --train
+	--opt_limit_datapoints 100000 \
+	--opt_codebits 0 \
+	--train
